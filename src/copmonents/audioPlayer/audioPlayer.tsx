f11d1926387ca55f0e38dc980f8aea60fa247366
Skave-a <arrowtata@gmail.com>
2023-06-10 10:15:34 +0300

    fix: player

--- src/copmonents/audioPlayer/audioPlayer.tsx
+++ src/copmonents/audioPlayer/audioPlayer.tsx
@@ -2,12 +2,22 @@ import { formatTime } from '@/utils';
 import styles from './audioPlayer.module.scss';
 import play from '@/assets/player/play.svg';
 import pause from '@/assets/player/pause.png';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { getCallRecordAudio } from '../../app/api/api';
 
-export const AudioPlayer = ({ audio, time }: { audio: string | undefined; time: number }) => {
+export const AudioPlayer = ({
+  time,
+  record,
+  partnership_id,
+}: {
+  time: number;
+  record: string;
+  partnership_id: string;
+}) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [audio, setAudio] = useState<HTMLAudioElement>();
 
   const togglePlay = () => {
     const audioElement = audioRef.current;
@@ -26,9 +36,17 @@ export const AudioPlayer = ({ audio, time }: { audio: string | undefined; time: 
     }
   };
 
+  useEffect(() => {
+    async function fetchData() {
+      const audioUser = await getCallRecordAudio(record, partnership_id);
+      setAudio(audioUser);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.playerControls}>
-      <audio ref={audioRef} src={audio} onTimeUpdate={handleTimeUpdate} />
+      <audio ref={audioRef} src={audio?.src} onTimeUpdate={handleTimeUpdate} />
       <p style={{ marginRight: '12px', marginLeft: '19px' }}>{formatTime(time)}</p>
       {isPlaying ? (
         <img src={pause} alt="pause" onClick={togglePlay} className={styles.btnPlayer} />
